d100c7aa6f6817f1eae53869009287ba00eb5664
Nina Sitaeva <sitaeva.nn@gmail.com>
2021-09-07 23:44:20 +0300

    feat: add dealer block on lobby page

---
+++ src/members-data.ts
@@ -0,0 +1,50 @@
+export const players = [
+  {
+    firstName: "Rick",
+    lastName: "Putin",
+    jobPosition: "Ingeneer",
+    gameRole: "player",
+  },
+  {
+    firstName: "Nick",
+    lastName: "Braian",
+    jobPosition: "Tester",
+    gameRole: "player",
+  },
+  {
+    firstName: "Carl",
+    lastName: "Luciano",
+    jobPosition: "Project Manager",
+    gameRole: "dealer",
+  },
+  {
+    firstName: "Vasilisa",
+    lastName: "Ivanova",
+    jobPosition: "Analyst",
+    gameRole: "player",
+  },
+  {
+    firstName: "Kate",
+    lastName: "Middleton",
+    jobPosition: "Ingeneer",
+    gameRole: "player",
+  },
+  {
+    firstName: "Alexander",
+    lastName: "Alexander",
+    jobPosition: "Ingeneer",
+    gameRole: "player",
+  },
+  {
+    firstName: "Coralla",
+    lastName: "Covid",
+    jobPosition: "Ingeneer",
+    gameRole: "player",
+  },
+  {
+    firstName: "Lera",
+    lastName: "Putin",
+    jobPosition: "Ingeneer",
+    gameRole: "player",
+  },
+];
