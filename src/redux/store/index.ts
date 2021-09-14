2f387b4f90b361ad6cdb720b4b176580fb089c5b
Bekzhan Zhamantayev <bekzzhan31@gmail.com>
2021-09-14 16:50:03 +0600

    refactor: delete useless redux files

--- src/redux/store/index.ts
+++
@@ -1,6 +0,0 @@
-import { composeWithDevTools } from 'redux-devtools-extension';
-import { applyMiddleware, createStore } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers/index';
-
-export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
--- src/redux/store/reducers/index.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import mainReducer from "../../../redux/store/reducers/mainReducer";
-
-
-const rootReducer = combineReducers({
-  mainReducer,
-});
-export default rootReducer;
