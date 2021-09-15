da706d8339fdfbc82c6104967b21c4e693df1152
Nina Sitaeva <sitaeva.nn@gmail.com>
2021-09-15 04:11:32 +0300

    fix: styles for buttons and scrum master bage

--- src/pages/Lobby/components/Button/Button.tsx
+++ src/pages/Lobby/components/Button/Button.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import "./Button.css";
 
 const Button = (props: any) => {
-  return <button className="blue-btn">{props.text}</button>;
+  return <button className={props.class}>{props.text}</button>;
 };
 
 export default Button;
