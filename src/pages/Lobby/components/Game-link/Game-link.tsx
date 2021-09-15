c022fb9f3f0b0a69c6631939a838c0698e0d359e
Nina Sitaeva <sitaeva.nn@gmail.com>
2021-09-15 04:26:41 +0300

    feat: add copy game link function

--- src/pages/Lobby/components/Game-link/Game-link.tsx
+++ src/pages/Lobby/components/Game-link/Game-link.tsx
@@ -4,15 +4,24 @@ import Button from "../Button/Button";
 import "./Game-link.scss";
 
 const GameLink = () => {
-  const handleCopyLink = () => {
-    console.log("click");
+  //TO DO нужно менять линк на игру
+  const link = "http://pockerplanning.com/111";
+  const handleCopyLink = (e: any) => {
+    e.preventDefault();
+    e.stopPropagation();
+    navigator.clipboard
+      .writeText(link)
+      .then(() => console.log("Game link copied"))
+      .catch((err) => {
+        console.log("Something went wrong", err);
+      });
   };
 
   return (
     <div className="game-link">
       <h2>Link to lobby:</h2>
       <form className="link">
-        <input type="text" value="http://pockerplanning.c..."></input>
+        <input type="text" value={link}></input>
         <Button onClick={handleCopyLink} text="Copy" class="blue-btn" />
       </form>
     </div>
