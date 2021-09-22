35da605d15da489ea50b7f476c3d20cf19afe8a3
Nina Sitaeva <sitaeva.nn@gmail.com>
2021-09-23 02:17:04 +0300

    votes for voting table

--- src/pages/GamePage/components/Game-vote/Vote-row/Vote-row.tsx
+++ src/pages/GamePage/components/Game-vote/Vote-row/Vote-row.tsx
@@ -1,20 +1,14 @@
 import React from "react";
-import Member from "../../../../../pages/Lobby/components/Member-bage/Member-bage";
+import { Member } from "../../../../../components/Member/Member";
 import { IMember } from "../../../../../ts/interfaces/app_interfaces";
 
 import "./Vote-row.scss";
 
 const VoteRow = ({ player, score }: any) => {
-  console.log(player);
   return (
     <div className="vote-row">
       <div className="vote-score">{score ? score : "unknown"}</div>
-      <Member
-        firstName={player.firstName}
-        lastName={player.lastName}
-        jobPosition={player.jobPosition}
-        gameRole={player.gameRole}
-      />
+      <Member member={player} />
     </div>
   );
 };
