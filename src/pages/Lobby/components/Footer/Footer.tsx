6f3c48d8ef593be06c52ff9a59f44a26ad7cd365
Nina Sitaeva <sitaeva.nn@gmail.com>
2021-09-24 22:10:12 +0300

    fix: changed link in footer for our team link

--- src/pages/Lobby/components/Footer/Footer.tsx
+++ src/pages/Lobby/components/Footer/Footer.tsx
@@ -6,11 +6,27 @@ const Footer = () => {
   return (
     <footer className="lobby-footer">
       <div className="footer-line">
-          <div className="footer-background">
-              <div><a href="https://rs.school/"><img className="footer-photo" src={require('../../../../../assets/rsschool.svg')} alt="it's a logo" /></a></div>
-              <div className="footer__year">2021</div>
-              <div><a href="https://github.com/nina-si"><img className="footer-github" src={require('../../../../../assets/github.svg')} alt="it's a github" /></a></div>
+        <div className="footer-background">
+          <div>
+            <a href="https://rs.school/">
+              <img
+                className="footer-photo"
+                src={require("../../../../../assets/rsschool.svg")}
+                alt="it's a logo"
+              />
+            </a>
           </div>
+          <div className="footer__year">2021</div>
+          <div>
+            <a href="https://github.com/Team-poker">
+              <img
+                className="footer-github"
+                src={require("../../../../../assets/github.svg")}
+                alt="it's a github"
+              />
+            </a>
+          </div>
+        </div>
       </div>
     </footer>
   );
