9a7da0d457b395e6ccdf8b59e16ae422fdffa22d
Nina Sitaeva <sitaeva.nn@gmail.com>
2021-09-24 20:11:34 +0300

    feat: add taking scrum master info from store

--- src/pages/Lobby/components/Scrum-master/Scrum-master.tsx
+++ src/pages/Lobby/components/Scrum-master/Scrum-master.tsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { connect } from "react-redux";
 
 import { players } from "../../../../members-data";
 import "./Scrum-master.css";
 
 // TO DO: если с бэка пришло фото - отображать фото вместо инициалов в блоке dealer-picture
-const ScrumMasterBlock = () => {
-  const dealer = players.find((player) => player.gameRole === "dealer");
-  const initials = dealer.firstName.charAt(0) + dealer.lastName.charAt(0);
+const ScrumMasterBlock = (props: any) => {
+  // const dealer = players.find((player) => player.gameRole === "dealer");
+  const initials =
+    props.currentUser.firstName.charAt(0) +
+    props.currentUser.lastName.charAt(0);
 
   return (
     <div className="scrum-master">
@@ -17,12 +20,18 @@ const ScrumMasterBlock = () => {
         </div>
         <div className="dealer-info">
           <span className="dealer-label">It's you</span>
-          <p className="dealer-name">{`${dealer.firstName} ${dealer.lastName}`}</p>
-          <span className="dealer-title">{dealer.jobPosition}</span>
+          <p className="dealer-name">{`${props.currentUser.firstName} ${props.currentUser.lastName}`}</p>
+          <span className="dealer-title">{props.currentUser.jobPosition}</span>
         </div>
       </div>
     </div>
   );
 };
 
-export default ScrumMasterBlock;
+const mapStateToProps = (state: any) => {
+  return {
+    currentUser: state.currentUser,
+  };
+};
+
+export default connect(mapStateToProps, null)(ScrumMasterBlock);
