b48668c09fe97b4fbe604532a203025b5560a404
Klimova Kseniya <ksuoxy773@gmail.com>
2021-10-05 16:13:06 +0400

    fix: change gametimer

--- src/pages/GamePage/components/Game-info/Round-controls/Round-controls.tsx
+++ src/pages/GamePage/components/Game-info/Round-controls/Round-controls.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Button from "../../../../Lobby/components/Button/Button";
-import GameTimer from "../Game-state/Game-timer/Game-timer";
+// import GameTimer from "../Game-state/Game-timer/Game-timer";
 
 import "./Round-controls.scss";
 
@@ -13,14 +13,14 @@ const RoundControls = () => {
     <section className="round-controls">
       {isNewRound && (
         <div className="new-round-control">
-          <GameTimer timer={425} />
+          {/*<GameTimer timer={425} />*/}
           <Button className="blue-btn" text="Run Round" />
         </div>
       )}
       {!isNewRound && (
         <>
           <div className="reset-round-control">
-            <GameTimer />
+            {/*<GameTimer />*/}
             <Button class="blue-btn" text="Restart Round" />
           </div>
           <Button class="blue-btn" text="Next Issue" />
